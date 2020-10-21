109c2e779862cde8b5cbecc94e7f95cc9cca0cc2
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-10-21 15:37:14 -0400

    [js]Add object to .js file

---
+++ star_wars.js
@@ -0,0 +1,22 @@
+// const iphone12 = {
+//     image: 'path to where the image is located',
+//     length: '11:28',
+//     title: 'iPhone 12 Unboxing Experience + MagSafe',
+//     username: 'Marques Brownlee',
+//     views: '5.5M',
+//     uploaded: 15
+// };
+
+// console.log(iphone12.title);
+
+const poster =  {
+    type: "screenshot",
+    company: "Disney",
+    title: "Special Look | The Mandalorian | Disney+",
+    user: "Star Wars",
+    views: "1M",
+    posted: "1 day ago",
+    length: "1:00",
+}
+
+console.log("This video has a length of "+ poster.length + " and was posted "+ poster.posted);
